6aa11693412bc05f1db82b99fbc6daeba8ce802e
Jonathan Glanz <jonathan.glanz@gmail.com>
2025-09-29 22:08:12 -0400

    OPS-15: Removed explicit `gcc-10` compiler flags from wire-sys-vm

--- ports/wire-sys-vm/portfile.cmake
+++ ports/wire-sys-vm/portfile.cmake
@@ -4,9 +4,6 @@ vcpkg_from_git(
     REF 1f73ea61e25ad3f83d31639eb82b3b018802b957
 )
 
-find_program(GCC_10 gcc-10 REQUIRED)
-find_program(GXX_10 g++-10 REQUIRED)
-
 vcpkg_check_features(OUT_FEATURE_OPTIONS OPTIONS
     INVERTED_FEATURES
         softfloat-skip-install ENABLE_SOFTFLOAT_INSTALL
@@ -15,8 +12,6 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS OPTIONS
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
-    -DCMAKE_C_COMPILER=${GCC_10}
-    -DCMAKE_CXX_COMPILER=${GXX_10}
     -DENABLE_INSTALL=ON
     -DENABLE_TESTS=OFF
     -DENABLE_SPEC_TESTS=OFF
