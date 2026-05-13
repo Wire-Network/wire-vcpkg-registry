8b3d4f386d2ccae7ac4b8e58f9eea40ae09d19e0
Huang-Ming Huang <huangming.huang@gmail.com>
2026-05-13 15:34:31 -0500

    Update bn256 port to upstream CMake

--- ports/bn256/portfile.cmake
+++ ports/bn256/portfile.cmake
@@ -3,17 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH
   URL https://github.com/Wire-Network/bn256.git
-  REF 88b32240b6362cac501fbec4d53021355722883f
+  REF a278132c53a1475b09d62351ba1028df36903291
 )
-
-file(COPY_FILE "${CMAKE_CURRENT_LIST_DIR}/bn256-CMakeLists.txt.cmake" "${SOURCE_PATH}/CMakeLists.txt")
-file(COPY_FILE "${CMAKE_CURRENT_LIST_DIR}/bn256-src-CMakeLists.txt.cmake" "${SOURCE_PATH}/src/CMakeLists.txt")
-
-# vcpkg_execute_required_process(
-#   COMMAND sed -i "/third-party/d" CMakeLists.txt
-#   WORKING_DIRECTORY ${SOURCE_PATH}
-#   LOGNAME bn256-remove-thirdparty
-# )
 
 set(EXTRA_OPTIONS)
 if (CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" OR CMAKE_SYSTEM_PROCESSOR STREQUAL "AMD64")
@@ -29,6 +20,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}" PACKAGE_NAME ${PORT})
 
 if (EXISTS ${CURRENT_PACKAGES_DIR}/debug/include)
   file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
@@ -37,20 +29,5 @@ endif()
 #   file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 # endif()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-
-# file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cmake/${PORT}")
-# file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/lib/cmake/${PORT}")
-# configure_file(
-#   "${CMAKE_CURRENT_LIST_DIR}/bn256Config.cmake.in"
-#   "${CURRENT_PACKAGES_DIR}/lib/cmake/${PORT}/bn256Config.cmake"
-#   @ONLY
-# )
-
-
-# vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
-# PACKAGE_NAME ${PORT}
-
-
-
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
