edc5d76d3f71ee096c8a463b203656bed9120e84
Huang-Ming Huang <huangming.huang@gmail.com>
2026-05-25 16:29:02 -0500

    Update wire-sys-vm for renamed SoftFloat types

--- ports/wire-sys-vm/portfile.cmake
+++ ports/wire-sys-vm/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/Wire-Network/eos-vm
-    REF 31ad8527c0d9f124e7606d0899ab0477c9965551
+    REF 09cf11c77f7869f6192cff14006435681ec94c3d
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS OPTIONS
